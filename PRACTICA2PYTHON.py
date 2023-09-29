b1b0231e98548cbb38e5329aa340ef25e611b3c4
EduardoVP-0 <144479949+EduardoVP-0@users.noreply.github.com>
2023-09-28 23:35:32 -0600

    PRINT Y COMENTARIOS

--- PRACTICA2PYTHON.py
+++ PRACTICA2PYTHON.py
@@ -1,11 +1,18 @@
+#PRACTICA 4
+#Eduardo Velazquez
 maxf = 3
 maxc = 5
 a = [[0.0] * maxc for _ in range(maxf)]
 
+print("LSC 3J")
+#Escribir el array
 for f in range(maxf):
     for c in range(maxc):
+        print("Ingrese el valor: ")
         a[f][c]=float(input())
 
+#Leer el array
+print("Los valores del arreglo son los siguientes: ")
 for f in range(maxf):
     for c in range(maxc):
         print(a[f][c],end=" ")
